b9817c4ce8e84476a28ffbdb49967cce909e436b
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2023-06-28 14:13:33 -0400

    added data type RKey and constructor

--- src/Sound/Tidal/KeyChords.hs
+++ src/Sound/Tidal/KeyChords.hs
@@ -6,13 +6,31 @@ module Sound.Tidal.KeyChords where
    a lot of music theory.
 -}
 
+import Data.Maybe
 import Sound.Tidal.Types
+import Sound.Tidal.Scales
 
 -- RChord type
+-- includes the root of the chord
+-- and a list of notes in the chord
 data RChord = RChord { root :: Note
                      , noteList :: [Note]
                      } deriving (Show)
 
+-- RKey type
+-- includes the tonic and the "scale" (mode)
+data RKey = RKey { tonic :: Note
+                 , mode :: [Double] -- TODO: figure out how to have "Fractional a => [a]" here, to match Scales.hs
+                 } deriving (Show)
+
+-- strRKey takes a Note ton as the tonic
+-- and a String modeStr with the name of a scale (mode)
+-- and contructs an RKey
+strRKey :: Note -> String -> RKey
+strRKey ton modeStr = if jMode /= Nothing -- make sure the scale is findable
+                           then RKey ton (fromJust jMode)
+                           else error "Mode not available"
+                        where jMode = lookup modeStr scaleTable 
 
 -- keyChords takes a Pattern Int and converts it to a Pattern Note that will
 -- play the corresponding chords in the specified key.
