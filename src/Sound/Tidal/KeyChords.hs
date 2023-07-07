e9078e5fa92f1c387afb3b1d5cc27d501b3ffb63
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2023-07-07 13:55:31 -0400

    transition to DegChords

--- src/Sound/Tidal/KeyChords.hs
+++ src/Sound/Tidal/KeyChords.hs
@@ -7,80 +7,126 @@ module Sound.Tidal.KeyChords where
 -}
 
 import Data.Maybe
-import Data.List
 import Sound.Tidal.Types
 import Sound.Tidal.Scales
 
--- RChord type
--- includes the root of the chord
--- and a list of notes in the chord
--- entries in noteList should be in order of ascending pitch
-data RChord = RChord { root :: Note
-                     , noteList :: [Note]
+------ types, classes, and related functions ------
+
+-- Degree type
+-- for indicating scale degree when you don't know what scale you are using
+-- includes the degree and an octave adjustment
+data Degree = Degree { deg :: Int
+                     , octs :: Int
                      } deriving (Show)
 
--- RKey type
+-- degAdd function to easily add to the degree field of a Degree
+degAdd :: Degree -> Int -> Degree
+degAdd d i = Degree {deg = newDeg, octs = newOcts}
+             where
+                newDeg = deg d + i
+                newOcts = octs d
+
+-- octAdd function to easily add to the octaves field of a Degree
+octAdd :: Degree -> Int -> Degree
+octAdd d i = Degree {deg = newDeg, octs = newOcts}
+             where
+                newDeg = deg d
+                newOcts = octs d + i
+
+-- degrees can be compared
+instance Eq Degree where 
+   (Degree d1 o1) == (Degree d2 o2) = (d1 == d2) && (o1 == o2)
+
+-- DegChord type
+-- a chord of scale degrees
+-- includes the root of the chord
+-- and a list of the other scale degrees in the chord
+data DegChord = DegChord { degRoot :: Degree
+                         , degList :: [Degree]
+                         } deriving (Show)
+
+-- Key type
 -- includes the tonic and the "scale" (mode)
-data RKey = RKey { tonic :: Note
-                 , mode :: [Double] -- TODO: figure out how to have "Fractional a => [a]" here, to match Scales.hs
-                 } deriving (Show)
+data Key = Key { tonic :: Note
+               , mode :: [Double] -- TODO: figure out how to have "Fractional a => [a]" here, to match Scales.hs
+               } deriving (Show)
 
--- strRKey takes a Note ton as the tonic
+-- another constructor for Keys
+-- strKey takes a Note ton as the tonic
 -- and a String modeStr with the name of a scale (mode)
--- and contructs an RKey
-strRKey :: Note -> String -> RKey
-strRKey ton modeStr = if jMode /= Nothing -- make sure the scale is findable
-                         then RKey {tonic = ton, mode = (fromJust jMode)}
+-- and contructs a Key
+strKey :: Note -> String -> Key
+strKey ton modeStr = if jMode /= Nothing -- make sure the scale is findable
+                         then Key {tonic = ton, mode = (fromJust jMode)}
                          else error "Mode not available"
-                      where jMode = lookup modeStr scaleTable 
+                     where jMode = lookup modeStr scaleTable 
 
--- sclDegTriad takes an RKey key and an Int sclDeg
--- and returns an RChord which is a triad built in the given key
+-- KeyChord type
+-- includes the key,
+-- the root of the chord and its degree rootDeg,
+-- and a list of notes in the chord
+-- entries in noteList should be in order of ascending pitch
+data KeyChord = KeyChord { key :: Key
+                         , rootDeg :: Degree
+                         , root :: Note
+                         , noteList :: [Note]
+                         } deriving (Show)
+
+
+-- DCMod type
+-- modifiers for DegChords
+data DCMod = Invert
+
+instance Show DCMod where
+   show Invert = "Invert"
+
+
+------ main functions ------
+
+-- degTriad takes a Degree sclDeg
+-- and returns a DegChord which is a triad built 
 -- on the given scale degree
--- notes in the noteList are in ascending order
-sclDegTriad :: RKey -> Int -> RChord
-sclDegTriad key sclDeg = RChord {root = r, noteList = nL}
-                         where
-                            -- find the root
-                            r = sclDegGetNote key sclDeg
-                            -- find the third and fifth
-                            third = sclDegGetNote key (sclDeg + 2)
-                            fifth = sclDegGetNote key (sclDeg + 4)
-                            -- put the noteList together
-                            nL = r:third:fifth:[]
-
--- sclDegGetNote takes an RKey key and an Int sclDeg
+-- entries in degList are in ascending order
+degTriad :: Degree -> DegChord
+degTriad sclDeg = DegChord { degRoot = sclDeg
+                           , degList = sclDeg:third:fifth:[]
+                           }
+                  where
+                     third = sclDeg `degAdd` 2
+                     fifth = sclDeg `degAdd` 4 
+                            
+-- sclDegGetNote takes a Key key and a Degree sclDeg
 -- and returns the Note corresponding to that scale degree in that key
 -- note that it pays attention to octaves,
--- so if the root of key is c4, and you ask for the 8th scale degree
+-- so if the key is c4 major, and you ask for the 8th scale degree
 -- sclDegGetNote will return c5
-sclDegGetNote :: RKey -> Int -> Note
-sclDegGetNote key sclDeg = Note $ (mode key)!!ind + 12*oct
+sclDegGetNote :: Key -> Degree -> Note
+sclDegGetNote k sclDeg = Note $ (mode k)!!ind + 12*oct
                            where
-                              len = length $ mode key
-                              -- (sclDeg - 1) because lists are indexed from 0,
+                              len = length $ mode k
+                              d = deg sclDeg
+                              -- (deg - 1) because lists are indexed from 0,
                               -- but scale degree is traditionally indexed
                               -- from 1
-                              oct = fromIntegral $ (sclDeg - 1) `div` len -- octave adjustment
-                              ind = (sclDeg - 1) `mod` len -- index in the mode
+                              oct = fromIntegral $ ((d - 1) `div` len)
+                                       + (octs sclDeg) -- octave adjustment
+                              ind = (d - 1) `mod` len -- index in the mode
 
--- invertRChord takes the lowest pitch of an RChord and raises it an octave,
--- then resorts the noteList to maintain the ascending pitch order 
-invertRChord :: RChord -> RChord
-invertRChord chord = RChord {root = r, noteList = nL}
+-- invertDChord takes the first entry in the degList of a DegChord,
+-- raises it an octave, and moves it to the end of the degList
+invertDChord :: DegChord -> DegChord
+invertDChord chord = DegChord {degRoot = r, degList = dL}
                      where
-                        r = root chord -- the root stays the same
-                        -- put the lowest note in its own one-element list
-                        -- and the remaining notes in a separate list
-                        (lowest, remaining) = splitAt 1 (noteList chord)
-                        nL = sort $ remaining ++ [lowest!!0 + 12]
+                        -- grab the first degree and 
+                        -- put the remaining degrees in a separate list
+                        (first:remaining) = degList chord
+                        firstUp = first `octAdd` 1
+                        dL = remaining ++ [firstUp]
+                        -- the root stays the same unless it was the one that
+                        -- was raised an octave
+                        r = if first == degRoot chord
+                               then firstUp
+                               else degRoot chord
 
--- keyChords takes a Pattern Int and converts it to a Pattern Note that will
--- play the corresponding chords in the specified key.
--- For example,
---    d1 $ n keyChords "a" "major" "1 2 4 5" # s "superfm"
--- will play the I ii IV V chords in the key of A major,
--- i.e. Amaj, Bmin, Dmaj, Emaj
--- Note in particular that it is not necessary to know that the ii chord of a 
--- major key is minor in order to stay in the key.
+------ functions that interface with the parser ------
 
