9be545320c0e73c43046bf84da348ed190c0c48a
HoshiNoRandii <108156355+HoshiNoRandii@users.noreply.github.com>
2023-08-25 15:37:44 -0400

    introduce NoteChords

--- src/Sound/Tidal/KeyChords.hs
+++ src/Sound/Tidal/KeyChords.hs
@@ -20,25 +20,34 @@ import Sound.Tidal.Types   ( Note(..), ValueMap )
 
 ------ types, classes, and related functions ------
 
--- RChord type
--- an RChord stores a scale degree to build a chord from
+-- GenChord type
+-- a GenChord stores a scale degree to build a chord from
 -- as well as chord modifiers
-data RChord = RChord { sclDeg :: Int
-                     , mods :: [RCMod]
-                     } deriving (Show)
+data GenChord = GenChord { sclDeg :: Int
+                         , mods :: [NCMod]
+                         } deriving (Show)
 
--- RCMod type
--- modifiers for RChords
-data RCMod = RInvert | ROpen | RPower | RUp | RDown
-           | RAdd AddWhere (Maybe Int) Int deriving Eq
+-- NoteChord type
+-- a NoteChord stores a list of Notes
+-- as well as the root of the chord and the Key
+-- the noteList should be in ascending order
+data NoteChord = NoteChord { noteList :: [Note]
+                           , root :: Note
+                           , inKey :: Key
+                           } deriving(Show)
 
-instance Show RCMod where
-  show RInvert = "RChord Invert"
-  show ROpen = "RChord Open"
-  show RPower = "Power RChord"
-  show RUp = "RChord Octave Up"
-  show RDown = "RChord Octave Down"
-  show (RAdd x i st) = "RChord Add Scale Degree" ++ show i ++ "+" ++ show st ++ "semitones to" ++ show x
+-- NCMod type
+-- modifiers for NoteChords
+data NCMod = NInvert | NOpen | NPower | NUp | NDown
+           | NAdd AddWhere (Maybe Int) Int deriving Eq
+
+instance Show NCMod where
+  show NInvert = "NoteChord Invert"
+  show NOpen = "NoteChord Open"
+  show NPower = "Power NoteChord"
+  show NUp = "NoteChord Octave Up"
+  show NDown = "NoteChord Octave Down"
+  show (NAdd x i st) = "NoteChord Add Scale Degree" ++ show i ++ "+" ++ show st ++ "semitones to" ++ show x
 
 -- AddWhere type
 -- to indicate adding tones to the treble or bass of a chord
@@ -67,14 +76,14 @@ strKey ton modeStr
 
 ------ main functions ------
 
--- rChordToNotes takes an RChord and a Key and returns
--- a list of Notes corresponding to the triad built
--- on the scale degree indicated in the RChord in the
--- given Key, with the modifications indicated in the RChord.
--- The Note list should be sorted in ascending order
-rChordToNotes :: RChord -> Key -> [Note]
-rChordToNotes (RChord sd ms) key =
-  (flip applyRMods) ms $ sclDegKeyTriad sd key
+-- genToNoteChord takes a GenChord and a Key and returns
+-- the corresponding NoteChord, whose noteList corresponds to
+-- the triad build on the sclDeg in the given Key, with
+-- the modifications applied
+-- the noteList should be sorted in ascending order
+genToNoteChord :: GenChord -> Key -> NoteChord
+genToNoteChord (GenChord sd ms) key = (flip applyNCMods) ms $
+  NoteChord (sclDegKeyTriad sd key) (sclDegGetNote sd key) key
 
 -- sclDegKeyTriad takes an Int representing a scale degree
 -- and a Key and returns a list of three Notes corresponding
@@ -122,18 +131,18 @@ listIsMidiPlayable :: [Note] -> Bool
 listIsMidiPlayable ns = foldr f True ns
   where f n b = (isMidiPlayable n) && b
 
--- applyRMods takes a list of Notes and a list of RCMods
--- and applies the RMods to the list of Notes in the order
+-- applyNCMods takes a NoteChord and a list of NCMods
+-- and applies the NCMods to the noteList in the order
 -- they are listed
-applyRMods :: [Note] -> [RCMod] -> [Note]
-applyRMods ns [] = ns
-applyRMods ns (x:xs) = applyRMods (applyRMod ns x) xs
+applyNCMods :: NoteChord -> [NCMod] -> NoteChord
+applyNCMods nc [] = nc
+applyNCMods nc (m:ms) = applyNCMods (applyNCMod nc m) ms
 
 -- TODO: implement this lmao
--- applyRMod takes a list of Notes and an RCMod
--- and applies the RMod to the list of Notes
-applyRMod :: [Note] -> RCMod -> [Note]
-applyRMod ns rm = ns
+-- applyNCMod takes a NoteChord and an NCMod
+-- and applies the NCMod to the noteList
+applyNCMod :: NoteChord -> NCMod -> NoteChord
+applyNCMod nc m = nc
 
 
 ------ modifier functions ------
